bf2f541ba7337f5319a7d56871bff045dced4ff5
Joao Pedro <jpresende502@gmail.com>
2021-04-10 23:37:35 -0300

    refactor: Move the all functions of the controllers to one controller file.

--- lib/biblioteca_elixir_web/controllers/book_controller.ex
+++ lib/biblioteca_elixir_web/controllers/book_controller.ex
@@ -1,7 +1,16 @@
 defmodule BibliotecaElixirWeb.BookController do
   use BibliotecaElixirWeb, :controller
 
-  def view(conn, _params) do
-    render(conn, "books.json", books: BibliotecaElixir.view_book())
+  def view_books(conn, _params) do
+    render(conn, "books.json", books: BibliotecaElixir.view_books())
+  end
+
+  def view_book(conn, %{"id" => id}) do
+    render(conn, "book.json", book: BibliotecaElixir.view_book!(id))
+  end
+
+  def create(conn, params) do
+    book = BibliotecaElixir.create_book(params)
+    render(conn, "created.json", book: book)
   end
 end
--- lib/biblioteca_elixir_web/controllers/create_book_controller.ex
+++
@@ -1,8 +0,0 @@
-defmodule BibliotecaElixirWeb.CreateBookController do
-  use BibliotecaElixirWeb, :controller
-
-  def create(conn, params) do
-    book = BibliotecaElixir.create_book(params)
-    render(conn, "created.json", book: book)
-  end
-end
